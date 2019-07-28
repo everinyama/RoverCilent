b0bec04c0ff78f59c67823868c12046dfcd528a9
vedzah09 <52963364+vedzah09@users.noreply.github.com>
2019-07-28 09:37:33 +0200

    Update RoverClient.java

--- src/roverclient/RoverClient.java
+++ src/roverclient/RoverClient.java
@@ -5,12 +5,6 @@ package roverclient;
  * @author Vedzah
  */
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
-
 import clients.Client;
 
 
